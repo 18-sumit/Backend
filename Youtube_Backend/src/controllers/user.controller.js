7d138300065e0bf901fa6173719a3881c923ba46
Sumit Singh <sumitsingh3076@gmail.com>
2024-11-07 21:25:25 +0530

    basic testing of controller

--- Youtube_Backend/src/controllers/user.controller.js
+++ Youtube_Backend/src/controllers/user.controller.js
@@ -2,11 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler( async (req , res) => {
-    res.status(200).json(
-        {
-            message : "CHAI AUR CODE"
-        }
-    )
+    // steps:
+    // get user details from frontend
+    // validation - fields are not empty
+    // check if user already exists : check through username and email
+    // check for images & avatar
+    // if available upload them to cloudinary , check avatar uploaded or not
+    // create user object - create entry in DB\
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response else send error
+
+    // 1st step: get details 
+
+    const {fullname , email , username , password} = req.body
+    console.log("email : ", email)
+
 })
 
 
