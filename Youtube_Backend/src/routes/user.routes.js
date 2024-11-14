9c355af1996ae9de45635d918fea4c1ae6b11c12
Sumit Singh <sumitsingh3076@gmail.com>
2024-11-14 21:13:18 +0530

    middleware added for upload

--- Youtube_Backend/src/routes/user.routes.js
+++ Youtube_Backend/src/routes/user.routes.js
@@ -1,10 +1,30 @@
 import {Router} from "express"
 import { registerUser } from "../controllers/user.controller.js"
-
+import {upload} from '../middlewares/multer.middleware.js'
 
 const router = Router()
 
-router.route( "/register").post(registerUser)
+router.route( "/register").post(
+    // ye ek middleware h
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser
+
+)
+
+
+
+
+
+
 // yaha aake router.route("ye hai route").post(route pe jaane ke baad kya hoga wo method / controller yaha )
 // "/users" prefix ke baad ke main routes yaha likhe jayenge
 
