3f6afa314135cf922850457cb5208c0042dd665f
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-01 17:39:42 +0530

    new route added for login

--- Youtube_Backend/src/routes/user.routes.js
+++ Youtube_Backend/src/routes/user.routes.js
@@ -1,34 +1,33 @@
-import {Router} from "express"
-import { registerUser } from "../controllers/user.controller.js"
-import {upload} from '../middlewares/multer.middleware.js'
+import { Router } from "express"
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js"
+import { upload } from '../middlewares/multer.middleware.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
-router.route( "/register").post(
+router.route("/register").post(
     // ye ek middleware h
     upload.fields([
         {
-            name : "avatar",
-            maxCount : 1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name : "coverImage",
-            maxCount : 1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser
 
 )
-
-
-
-
-
-
 // yaha aake router.route("ye hai route").post(route pe jaane ke baad kya hoga wo method / controller yaha )
 // "/users" prefix ke baad ke main routes yaha likhe jayenge
 
+router.route("/login").post(loginUser)
 
+
+// secured routes 
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 export default router
