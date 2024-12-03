f169279b98d0d110017ebfbf44c048d0d86773a7
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-03 23:44:42 +0530

    Added new routes for userController

--- Youtube_Backend/src/routes/user.routes.js
+++ Youtube_Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar } from "../controllers/user.controller.js"
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -29,6 +29,27 @@ router.route("/login").post(loginUser)
 // secured routes 
 router.route("/logout").post(verifyJWT, logoutUser) // verifyJWT is a middleware here
 router.route("/refresh-token").post(refreshAccessToken)
+
+//only loggedIn user can do it so we are using verifyJWT 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+// as user is not sending any data so we can user get
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+// .patch() method is used in HTTP requests to update partial data on a resource. 
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+
+
+//Handling URL data : patch of partial update of avatar , and verifyJWT for user loggedIn , and upload.single form multer to get a single file instead of files
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// Handling data with params :
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
 
