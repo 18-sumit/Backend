9ad09dc0a4e35d493dca53daeb6b30e1c8f51d71
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-06 13:44:58 +0530

    updated

--- Youtube_Backend/src/controllers/user.controller.js
+++ Youtube_Backend/src/controllers/user.controller.js
@@ -200,7 +200,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // remove this , the field from the document
+                refreshToken: 1 // this removes the field from the document
             }
         },
         {
