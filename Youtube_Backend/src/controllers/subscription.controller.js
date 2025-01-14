9d558523a5ee2a7726fcdc56ef62d88f782a0c5a
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-14 23:20:55 +0530

    feat : Added subscribe function

--- Youtube_Backend/src/controllers/subscription.controller.js
+++ Youtube_Backend/src/controllers/subscription.controller.js
@@ -9,6 +9,62 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
     // TODO: toggle subscription
+    try {
+        if (isValidObjectId(channelId)) {
+            throw new ApiError(400, "Invalid channelId")
+        }
+
+        const channelid = await Subscription.findOne(channelId);
+        if (!channelid) {
+            throw new ApiError(
+                404,
+                "Channel not found"
+            );
+        }
+
+        if (!req.user || !req.user._id) {
+            throw new ApiError(
+                401,
+                "User not authenticated"
+            );
+        }
+
+        const subscribedAlready = await Subscription.findOne({
+            channel: channelId,
+            subscriber: req.user._id
+        });
+
+        if (subscribedAlready) {
+            await Subscription.findByIdAndDelete(subscribedAlready._id);
+
+            return res
+                .status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        { isSubscribed: false },
+                        "unsubscribed successfully"
+                    )
+                );
+        }
+
+        await Subscription.create({
+            channel: channelId,
+            subscriber: req.user._id
+        })
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    { isSubscribed: true },
+                    "subscribed successfully"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, `An error occurred: ${error.message}`);
+
+    }
 })
 
 // controller to return subscriber list of a channel
